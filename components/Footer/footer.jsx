e37b67dd32f874913f03da540eaa3053d68e7515
Haxxenigma <136870545+Haxxenigma@users.noreply.github.com>
2023-12-12 16:03:46 +0600

    Update footer.jsx

--- components/Footer/footer.jsx
+++ components/Footer/footer.jsx
@@ -1,13 +1,12 @@
-import Link from 'next/link';
 import styles from './footer.module.scss';
 
 export default function Footer() {
     return (
         <footer className={styles.footer}>
             <nav className={styles.nav}>
-                <Link href={'terms'}>Terms</Link>
-                <Link href={'privacy'}>Privacy</Link>
-                <Link href={'copyright'}>Copyright</Link>
+                <a href={'https://vercel.com/haxxenigmas-projects/web-app'} target='_blank'>Project</a>
+                <a href={'mailto:nkomyt@gmail.com'} target='_blank'>Email Me</a>
+                <a href={'https://github.com/Haxxenigma/'} target='_blank'>Source Code</a>
             </nav>
             <div className={styles.end}>
                 &copy; Haxxenigma &middot; 2023-2024
