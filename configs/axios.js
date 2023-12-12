146410fcb3bad306dd5370fa18d138aad046a3aa
Haxxenigma <136870545+Haxxenigma@users.noreply.github.com>
2023-12-12 16:30:33 +0600

    Update axios.js

--- configs/axios.js
+++ configs/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const createAxiosInstance = axios.create({
-    baseURL: 'http://localhost:3000/api',
+    baseURL: '/api',
     timeout: 5000,
     headers: {
         'Content-Type': 'application/json',
